a325a73939d2d8e85469cfbb38f4a1a7198ea99b
Jim Greenleaf <james.a.greenleaf@gmail.com>
2012-01-10 13:53:38 -0500

    lasers pewpew (space bar), no collisions yet

--- main.lua
+++ main.lua
@@ -1,20 +1,20 @@
 -- <3
 function love.load()
+    enemies = {}
+    projectiles = {}
     screen = {}
     screen.width = love.graphics.getWidth( )
     screen.height = love.graphics.getHeight( )
     
-    zig = pewpew.makeUnit('zig', screen.width / 2, screen.height / 2);
-    
-    enemies = {}
+    pewpew.spawnUnit('zig', screen.width / 2, screen.height / 2)
     for i=1,3 do
-        enemies[i] = pewpew.makeUnit('raven', i * 20 + 30, i * 20 + 30)
-        enemies[i].direction = (i % 2 > 0) and 'right' or 'left'
+        pewpew.spawnUnit('raven', i * 20 + 30, i * 20 + 30)
     end
 end
 function love.draw()
     pewpew.moveZig( )
     pewpew.moveEnemies( )
+    pewpew.moveProjectiles( )
     
     love.graphics.draw(zig.image, zig.x, zig.y, 0, 1, 1, zig.ox, zig.oy)
     for i, enemy in ipairs(enemies) do
@@ -22,22 +22,35 @@ function love.draw()
     end
     
     --random rectangle
-    love.graphics.setLineStyle('rough')
+    love.graphics.setLine( 1, 'rough' )
     love.graphics.setColorMode('replace')
     love.graphics.setColor(0, 0, 147, 150)
     love.graphics.rectangle('line', (zig.x - zig.ox) - 5, (zig.y - zig.oy) - 5, zig.width + 10, zig.height + 10)
 end
+function love.keypressed(key, u)
+    if key == ' ' then
+        pewpew.spawnProjectile('laser', zig.x + zig.ox, zig.y, 'up')
+    end
+end
+
 
 
 pewpew = {}
-function pewpew.makeUnit(type, x, y)
+function pewpew.spawnUnit(type, x, y)
     local unit = {}
     
-    ship_images = {
-      ['zig'] = 'images/zig.gif',
-      ['raven'] = 'images/enemy_black.gif'
+    unit_types = {
+       zig = {
+           image = 'images/zig.gif'
+          ,role = 'player'
+      }
+      ,raven = {
+           image = 'images/enemy_black.gif'
+          ,role = 'enemy'
+      }
     }
-    unit.image = love.graphics.newImage(ship_images[type])
+    
+    unit.image = love.graphics.newImage(unit_types[type].image)
     
     unit.width = unit.image:getWidth( )
     unit.height = unit.image:getHeight( )
@@ -48,7 +61,26 @@ function pewpew.makeUnit(type, x, y)
     unit.x = x or unit.ox
     unit.y = y or unit.oy
     
-    return unit
+    if unit_types[type].role == 'player' then
+        zig = unit
+    end
+    
+    if unit_types[type].role == 'enemy' then
+        unit.direction = (#enemies % 2 > 0) and 'right' or 'left'
+        enemies[#enemies + 1] = unit
+    end
+end
+function pewpew.spawnProjectile(type, x, y, direction)
+    local projectile = {
+         type = type
+        ,direction = direction
+        ,line_x = x
+        ,line_y = y
+        ,line_x1 = x
+        ,line_y1 = y - 10
+    }
+    
+    projectiles[#projectiles + 1] = projectile
 end
 
 function pewpew.moveZig()
@@ -78,8 +110,6 @@ function pewpew.moveEnemies()
             else
                 enemy.direction = 'left' 
             end
-            
-            --break
         end
 
         if enemy.direction == 'left' then
@@ -88,8 +118,20 @@ function pewpew.moveEnemies()
             else
                 enemy.direction = 'right'
             end
-            
-            --break
+        end
+    end
+end
+function pewpew.moveProjectiles( )
+    for i, p in ipairs(projectiles) do
+        if p.type == 'laser' then
+            love.graphics.setLine( 3, 'rough' )
+            love.graphics.setColor( 255, 0, 0, 255 )
+            love.graphics.line( p.line_x, p.line_y, p.line_x1, p.line_y1 )
+        end
+        
+        if p.direction == 'up' then
+            p.line_y = p.line_y - 4
+            p.line_y1 = p.line_y1 - 4
         end
     end
 end
